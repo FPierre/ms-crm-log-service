ca6f0b380c42f1989323fef5596768477bdd5546
pierre.flauder <pierre.flauder@seniormedia.fr>
2017-06-30 17:07:00 +0200

    Ajout d'une key sur le Responder et amélioration de la création des logs

--- log-service.js
+++ log-service.js
@@ -1,14 +1,14 @@
 const cote = require('cote')
-const responder = new cote.Responder({ name: 'log responder' })
-const publisher = new cote.Publisher({ name: 'log publisher', broadcasts: ['logUpdate'] })
+
+const responder = new cote.Responder({ name: 'log responder', key: 'log' })
+const publisher = new cote.Publisher({ name: 'log publisher', key: 'log', broadcasts: ['logUpdate'] })
 
 // DB mocking
-const logs = [
-  { id: 1, event: 'Agency creation', createdAt: new Date() }
-]
+let index = 0
+const logs = []
 
-responder.on('create', ({ type, log }, cb) => {
-  logs.push(log)
+responder.on('create', ({ type, event }, cb) => {
+  logs.push({ id: index++, event: event, createdAt: new Date()  })
   cb()
 })
 
