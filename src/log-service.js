5009c6e85df185acd7fde54f8a6f7173055c9ca4
FPierre <pflauder@gmail.com>
2017-07-16 11:55:19 +0200

    Methode de creation d'un log

--- src/log-service.js
+++ src/log-service.js
@@ -1,15 +1,17 @@
 const cote = require('cote')
+const { connect } = require('../db/connection')
+const { init } = require('../db/init')
+const Log = require('./log')
+
+connect()
+  // .then(() => init())
+  // .catch(err => console.log(err))
 
 const responder = new cote.Responder({ name: 'log responder', key: 'log' })
-const publisher = new cote.Publisher({ name: 'log publisher', key: 'log', broadcasts: ['logUpdate'] })
+const publisher = new cote.Publisher({ name: 'log publisher', key: 'log', broadcasts: ['logStream'] })
 
-// DB mocking
-let index = 0
-const logs = []
-
-responder.on('create', ({ type, event }, cb) => {
-  logs.push({ id: index++, event: event, createdAt: new Date() })
-  cb()
+responder.on('create', ({ event, model, note, user }) => {
+  return new Log({ event, model, note, _userId: user.id }).save()
 })
 
-setInterval(() => publisher.publish('logUpdate', logs), 2000)
+setInterval(() => publisher.publish('logStream', Log.find({})), 2000)
