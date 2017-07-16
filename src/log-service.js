a6d638a050105ff832e7c842af950aa9822ebfcf
FPierre <pflauder@gmail.com>
2017-07-16 11:59:17 +0200

    Fix erreur stream

--- src/log-service.js
+++ src/log-service.js
@@ -14,4 +14,6 @@ responder.on('create', ({ event, model, note, user }) => {
   return new Log({ event, model, note, _userId: user.id }).save()
 })
 
-setInterval(() => publisher.publish('logStream', Log.find({})), 2000)
+setInterval(() => {
+  publisher.publish('logStream', () => Log.find({}))
+}, 2000)
